5b8f347970dd212292dffb3f479359f46da55969
Weiss Bai <whitesgr03@gmail.com>
2024-09-13 19:36:57 +0800

    feat: use component Router instead of App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './components/pages/App';
-import './index.css';
+
+import { Router } from './Router';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<Router />
 	</StrictMode>,
 );
