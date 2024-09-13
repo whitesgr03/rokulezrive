d95b2aef41c8a684ce40b07abdd3538db397e2ad
Weiss Bai <whitesgr03@gmail.com>
2024-09-13 18:49:42 +0800

    build: add html csp nonce

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { randomBytes } from 'node:crypto';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	html: {
+		cspNonce: randomBytes(16).toString('base64'),
+	},
+});
