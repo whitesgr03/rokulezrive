6f9b0448ea96c07af47f4af66fd5724cd94cb7e7
Weiss Bai <whitesgr03@gmail.com>
2024-09-13 11:24:48 +0800

    feat: create Home component and module css file

---
+++ src/components/pages/Home/index.jsx
@@ -0,0 +1,88 @@
+// Packages
+import classNames from 'classnames/bind';
+
+// Styles
+import styles from './Home.module.css';
+
+// Assets
+import shareImage from '../../../assets/share.svg';
+import manageImage from '../../../assets/manage.svg';
+
+// Variables
+const classes = classNames.bind(styles);
+
+export const Home = activeMobileNav => {
+	return (
+		<div className={styles.home}>
+			<div
+				className={`${styles.hero} ${classes({
+					'active-mobile-nav': activeMobileNav,
+				})}`}
+			>
+				<h2 className={styles['hero-title']}>
+					<span className={styles['hero-title-highlight']}>Local Drive</span>{' '}
+					easy to store your content
+				</h2>
+				<p className={styles['hero-content']}>
+					Manage file from your mobile device or computer and sharing file to
+					anyone.
+				</p>
+				<a className={styles['home-link']} href="/account/register">
+					Try Local Drive for free
+				</a>
+			</div>
+			<div className={styles.container}>
+				<div className={styles.introduce}>
+					<figure>
+						<div className={styles['introduce-image-wrap']}>
+							<img
+								src={shareImage}
+								alt="Introduce of Share File"
+								className={styles['introduce-image']}
+							/>
+						</div>
+						<figcaption className={styles.caption}>
+							Designed by
+							<a href="www.freepik.com"> freepik</a>
+						</figcaption>
+					</figure>
+					<div className={styles['introduce-content']}>
+						<h2>Share files and folders with your friend</h2>
+						<p>
+							Even people without an account can easily access your shared files
+							or folders.
+						</p>
+					</div>
+				</div>
+				<div className={styles.introduce}>
+					<figure>
+						<div className={styles['introduce-image-wrap']}>
+							<img
+								src={manageImage}
+								alt="Introduce of Manage File"
+								className={styles['introduce-image']}
+							/>
+						</div>
+						<figcaption className={styles.caption}>
+							Designed by
+							<a href="www.freepik.com"> freepik</a>
+						</figcaption>
+					</figure>
+					<div className={styles['introduce-content']}>
+						<h2>Be in manage of your folders</h2>
+						<p>
+							When sharing, your&apos;re still in control. You can set expired
+							links to limit who can access your files or folders.
+						</p>
+					</div>
+				</div>
+				<div className={styles.started}>
+					<h3>Ready to get started?</h3>
+					<a className={styles['home-link']} href="/account/register">
+						Sign up Local Drive
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
