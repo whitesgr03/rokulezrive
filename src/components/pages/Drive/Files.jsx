3aec69ac8c29dcbc88ef5ea132380630556f7750
Weiss Bai <whitesgr03@gmail.com>
2024-09-17 14:33:48 +0800

    feat: render list using map method instead of List component

--- src/components/pages/Drive/Files.jsx
+++ src/components/pages/Drive/Files.jsx
@@ -1,6 +1,12 @@
+// Packages
 import { useOutletContext } from 'react-router-dom';
+import { format } from 'date-fns';
 
-import { List } from './List';
+// Styles
+import driveStyles from './Drive.module.css';
+import listStyles from './List.module.css';
+import { icon } from '../../../styles/icon.module.css';
+
 
 export const Files = () => {
   const { files, menu, onActiveMenu } = useOutletContext();
@@ -8,12 +14,47 @@ export const Files = () => {
 	return (
 		<>
 			<h3>Files</h3>
-			<List
-				data={files}
-				type={'files'}
-				onActiveMenu={onActiveMenu}
-				menu={menu}
-			/>
+			<ul className={listStyles.list}>
+				{files.map(item => (
+					<li key={item.id} className={listStyles.file}>
+						<button className={listStyles['file-button']}>
+							<span className={`${icon} ${listStyles[item.type]}`} />
+							<div className={listStyles.container}>
+								<p className={listStyles['file-name']}>{item.name}</p>
+								<div className={listStyles.info}>
+									<div className={listStyles['file-wrap']}>
+										<span className={`${icon} ${listStyles.size}`} />
+										<span className={listStyles['file-content']}>
+											{item.size}
+										</span>
+									</div>
+									<div className={listStyles['file-wrap']}>
+										<span className={`${icon} ${listStyles.date}`} />
+										<span className={listStyles['file-content']}>
+											{format(item.createdAt, 'MMM d, y')}
+										</span>
+									</div>
+								</div>
+							</div>
+						</button>
+						<div className={listStyles.options}>
+							<button
+								onClick={() =>
+									onActiveMenu({
+										id: item.id,
+										button: 'option-button',
+										name: 'option-menu',
+									})
+								}
+								data-id={item.id}
+								data-button="option-button"
+							>
+								<span className={`${icon} ${listStyles.option}`} />
+							</button>
+						</div>
+					</li>
+				))}
+			</ul>
 		</>
 	);
 };
--- src/components/pages/Drive/Shared.jsx
+++ src/components/pages/Drive/Shared.jsx
@@ -1,6 +1,12 @@
+// Packages
 import { useOutletContext } from 'react-router-dom';
+import { format } from 'date-fns';
 
-import { List } from './List';
+// Styles
+import driveStyles from './Drive.module.css';
+import listStyles from './List.module.css';
+import { icon } from '../../../styles/icon.module.css';
+
 
 export const Shared = () => {
 	const { shared, menu, onActiveMenu } = useOutletContext();
@@ -8,12 +14,47 @@ export const Shared = () => {
 	return (
 		<>
 			<h3>Shared</h3>
-			<List
-				data={shared}
-				type={'shared'}
-				onActiveMenu={onActiveMenu}
-				menu={menu}
-			/>
+			<ul className={listStyles.list}>
+				{shared.map(item => (
+					<li key={item.id} className={listStyles.file}>
+						<button className={listStyles['file-button']}>
+							<span className={`${icon} ${listStyles[item.type]}`} />
+							<div className={listStyles.container}>
+								<p className={listStyles['file-name']}>{item.name}</p>
+								<div className={listStyles.info}>
+									<div className={listStyles['file-wrap']}>
+										<span className={`${icon} ${listStyles['share-by']}`} />
+										<span className={listStyles['file-content']}>
+											{item.owner}
+										</span>
+									</div>
+									<div className={listStyles['file-wrap']}>
+										<span className={`${icon} ${listStyles.date}`} />
+										<span className={listStyles['file-content']}>
+											{format(item.createdAt, 'MMM d, y')}
+										</span>
+									</div>
+								</div>
+							</div>
+						</button>
+						<div className={listStyles.options}>
+							<button
+								onClick={() =>
+									onActiveMenu({
+										id: item.id,
+										button: 'option-button',
+										name: 'option-menu',
+									})
+								}
+								data-id={item.id}
+								data-button="option-button"
+							>
+								<span className={`${icon} ${listStyles.option}`} />
+							</button>
+						</div>
+					</li>
+				))}
+			</ul>
 		</>
 	);
 };
