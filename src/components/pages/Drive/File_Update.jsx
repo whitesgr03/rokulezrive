bdcc4177236da2be78be61f9cefe21e5dfbfbd71
Weiss Bai <whitesgr03@gmail.com>
2024-09-17 14:47:04 +0800

    feat: create Update and Delete of File component

---
+++ src/components/pages/Drive/File_Update.jsx
@@ -0,0 +1,51 @@
+// Packages
+import classNames from 'classnames/bind';
+
+// Styles
+import { icon } from '../../../styles/icon.module.css';
+import formStyles from '../../../styles/form.module.css';
+import modalStyles from '../App/Modal.module.css';
+
+// Variables
+const classes = classNames.bind(formStyles);
+
+export const File_Update = ({ name }) => {
+  const fileName = name;
+	const error = '';
+
+	return (
+		<form className={formStyles.form}>
+			<div >
+				<label htmlFor="file_rename" className={formStyles['form-label']}>
+					Rename
+					<input
+						type="text"
+						id="file_rename"
+						className={`${classes({
+							'form-input': true,
+							'form-input-error': error,
+						})} ${modalStyles['modal-input']}`}
+						name="file_rename"
+						value={fileName}
+						title="The file name is required."
+					/>
+				</label>
+				<div
+					className={classes({
+						'form-message-wrap': true,
+						'form-message-active': error,
+					})}
+				>
+					<span className={`${icon} ${formStyles.alert}`} />
+					<p className={formStyles['form-message']}>
+						{error ? error : 'Message Placeholder'}
+					</p>
+				</div>
+			</div>
+
+			<button type="submit" className={formStyles['form-submit']}>
+				Save
+			</button>
+		</form>
+	);
+};
