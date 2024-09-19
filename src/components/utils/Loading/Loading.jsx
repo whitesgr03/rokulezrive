99c4694fb87fddb5c222cc3740c2973e64226a4f
Weiss Bai <whitesgr03@gmail.com>
2024-09-19 09:52:49 +0800

    feat: receives props for custom text content and color

--- src/components/utils/Loading/Loading.jsx
+++ src/components/utils/Loading/Loading.jsx
@@ -1,12 +1,30 @@
+// Packages
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+
 // Styles
 import styles from './Loading.module.css';
 import { icon } from '../../../styles/icon.module.css';
 
-export const Loading = () => {
+// Variables
+const classes = classNames.bind(styles);
+
+export const Loading = ({ text, dark, light }) => {
 	return (
-		<div className={styles.loading}>
+		<div
+			className={`${styles.loading} ${classes({
+				dark,
+				light,
+			})}`}
+		>
 			<span className={`${icon} ${styles.load}`} />
-			Loading ...
+			{text}
 		</div>
 	);
+};
+
+Loading.propTypes = {
+	text: PropTypes.string,
+	dark: PropTypes.bool,
+	light: PropTypes.bool,
 };
