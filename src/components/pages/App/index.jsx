4394580196822ea31a188dfa86dbd4aea106260d
Weiss Bai <whitesgr03@gmail.com>
2024-09-19 09:57:07 +0800

    feat: add useEffect hook to get information about logged in user

--- src/components/pages/App/index.jsx
+++ src/components/pages/App/index.jsx
@@ -1,16 +1,20 @@
 // Packages
 import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 
 // Styles
 import styles from './App.module.css';
+
+// Utils
+import { handleFetch } from '../../../utils/handleFetch';
 
 // Components
 import { Header } from '../../layout/Header';
 import { Footer } from '../../layout/Footer';
 import { Mobile_nav } from '../../layout/Mobile_nav';
 import { Modal } from './Modal';
+import { Loading } from '../../utils/Loading/Loading';
 
 // Variables
 const classes = classNames.bind(styles);
@@ -19,12 +23,15 @@ const DEFAULT_MENU = {
 	button: '',
 	name: '',
 };
+const URL = `${import.meta.env.VITE_RESOURCE_URL}/account/user`;
 
 export const App = () => {
 	const [user, setUser] = useState(null);
 	const [darkTheme, setDarkTheme] = useState(false);
 	const [menu, setMenu] = useState(DEFAULT_MENU);
 	const [modal, setModal] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
 	const handleCloseMenu = e => {
 		const { id, button, closeMenu } = e.target.dataset;
@@ -73,49 +80,94 @@ export const App = () => {
 		getColorTheme();
 	}, []);
 
+	useEffect(() => {
+		const controller = new AbortController();
+		const { signal } = controller;
+
+		const handleGetUser = async () => {
+			const options = {
+				method: 'GET',
+				signal,
+				credentials: 'include',
+			};
+
+			const result = await handleFetch(URL, options);
+
+			const handleSuccess = () => {
+				result.success ? setUser(result.data) : setError(result.message);
+				setLoading(false);
+			};
+
+			result && handleSuccess();
+		};
+
+		const sessionExp = localStorage.getItem('drive.session-exp') ?? false;
+
+		const removeExp = () => {
+			localStorage.removeItem('drive.session-exp');
+			setLoading(false);
+		};
+
+		!sessionExp || Date.now() > new Date(JSON.parse(sessionExp)).getTime()
+			? removeExp()
+			: handleGetUser();
+
+		return () => controller.abort();
+	}, []);
+
 	return (
-		<div
-			className={`${styles.app} ${classes({
-				dark: darkTheme,
-				'active-mobile-nav': user,
-			})}`}
-			onClick={handleCloseMenu}
-		>
-			{modal && (
-				<Modal
-					onActiveModal={handleActiveModal}
-					onCloseModal={handleCloseModal}
+		<>
+			{loading ? (
+				<Loading text={'Loading...'} />
+			) : error ? (
+				<Navigate to="/error" state={{ error }} />
+			) : (
+				<div
+					className={`${styles.app} ${classes({
+						dark: darkTheme,
+						'active-mobile-nav': user,
+					})}`}
+					onClick={handleCloseMenu}
 				>
-					{modal}
-				</Modal>
-			)}
-			<Header
-				user={user}
-				darkTheme={darkTheme}
-				menu={menu}
-				onActiveMenu={handleActiveMenu}
-				onSwitchColorTheme={handleSwitchColorTheme}
-			/>
-			<div className={styles.container}>
-				<main>
-					<Outlet
-						context={{
-							user,
-							onActiveMenu: handleActiveMenu,
-							onActiveModal: handleActiveModal,
-							menu,
-						}}
+					{modal && (
+						<Modal
+							onActiveModal={handleActiveModal}
+							onCloseModal={handleCloseModal}
+						>
+							{modal}
+						</Modal>
+					)}
+					<Header
+						user={user}
+						darkTheme={darkTheme}
+						menu={menu}
+						onActiveMenu={handleActiveMenu}
+						onUser={setUser}
+						onSwitchColorTheme={handleSwitchColorTheme}
 					/>
-				</main>
-				<Footer />
-			</div>
-			{user && (
-				<Mobile_nav
-					menu={menu}
-					onActiveModal={handleActiveModal}
-					onActiveMenu={handleActiveMenu}
-				/>
+					<div className={styles.container}>
+						<main>
+							<Outlet
+								context={{
+									user,
+									onActiveMenu: handleActiveMenu,
+									onActiveModal: handleActiveModal,
+									onUser: setUser,
+									menu,
+								}}
+							/>
+						</main>
+						<Footer />
+					</div>
+					{user && (
+						<Mobile_nav
+							menu={menu}
+							onActiveModal={handleActiveModal}
+							onActiveMenu={handleActiveMenu}
+						/>
+					)}
+				</div>
 			)}
-		</div>
+		</>
 	);
 };
