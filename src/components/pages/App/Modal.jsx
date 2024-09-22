bd538a770b8aeb01ac4a264168d50855e22416b0
Weiss Bai <whitesgr03@gmail.com>
2024-09-22 17:18:51 +0800

    feat: the modal can only be closed when the clickToClose prop is true

--- src/components/pages/App/Modal.jsx
+++ src/components/pages/App/Modal.jsx
@@ -5,13 +5,19 @@ import PropTypes from 'prop-types';
 import styles from './Modal.module.css';
 import { icon } from '../../../styles/icon.module.css';
 
-export const Modal = ({ onCloseModal, children }) => {
+export const Modal = ({ onCloseModal, clickToClose, children }) => {
 	return (
-		<div className={styles.modal} onClick={onCloseModal} data-close-modal>
+		<div
+			className={styles.modal}
+			onClick={onCloseModal}
+			data-close-modal={clickToClose}
+		>
 			<div className={styles['modal-wrap']}>
-				<button className={styles['modal-button']} data-close-modal>
-					<span className={`${icon} ${styles.close}`} />
-				</button>
+				{clickToClose && (
+					<button className={styles['modal-button']} data-close-modal>
+						<span className={`${icon} ${styles.close}`} />
+					</button>
+				)}
 				<div className={styles.container}>{children}</div>
 			</div>
 		</div>
@@ -20,5 +26,6 @@ export const Modal = ({ onCloseModal, children }) => {
 
 Modal.propTypes = {
 	onCloseModal: PropTypes.func,
+	clickToClose: PropTypes.bool,
 	children: PropTypes.node,
 };
