a9bba579a0a0c20d84b913bcd6c6bdb56d209c11
Weiss Bai <whitesgr03@gmail.com>
2024-09-27 12:41:31 +0800

    feat: add "handleCreateSubfolder" function

--- src/components/pages/Drive/Folder/Folder.jsx
+++ src/components/pages/Drive/Folder/Folder.jsx
@@ -9,32 +9,65 @@ import { Subfolders } from './Subfolders';
 import { Files } from './Files';
 import { Folder_Create } from './Folder_Create';
 
+// Utils
+import { handleFetch } from '../../../../utils/handleFetch';
+
 export const Folder = () => {
-	const { folder, menu, onActiveMenu, onActiveModal } = useOutletContext();
+	const { folder, menu, onActiveMenu, onActiveModal, onGetFolder } =
+		useOutletContext();
+
+	const [error, setError] = useState(null);
+
+	const handleCreateSubfolder = async subfolder => {
+		const URL = `${import.meta.env.VITE_RESOURCE_URL}/api/folders`;
+
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ ...subfolder, parentId: folder.id }),
+			credentials: 'include',
+		};
+
+		const result = await handleFetch(URL, options);
+
+		const handleSuccess = () => {
+			onGetFolder();
+			onActiveModal({ component: null });
+		};
+
+		result.success
+			? handleSuccess()
+			: result.message && setError(result.message);
+
+		return result?.fields;
+	};
 
 	return (
-		<>
-			{!folder.children.length && !folder.files.length ? (
-				<p className={styles.text}>No files in the folder</p>
-			) : (
 				<>
-					{folder.children.length > 0 && (
-						<Subfolders
-							data={folder.children}
-							menu={menu}
-							onActiveMenu={onActiveMenu}
-							onActiveModal={onActiveModal}
-						/>
+					{!folder.children.length && !folder.files.length ? (
+						<p className={styles.text}>No files in the folder</p>
+					) : (
+						<>
+							{folder.children.length > 0 && (
+								<Subfolders
+									data={folder.children}
+									menu={menu}
+									onActiveMenu={onActiveMenu}
+									onActiveModal={onActiveModal}
+								/>
+							)}
+							{folder.files.length > 0 && (
+								<Files
+									data={folder.files}
+									menu={menu}
+									onActiveMenu={onActiveMenu}
+									onActiveModal={onActiveModal}
+								/>
+							)}
+						</>
 					)}
-					{folder.files.length > 0 && (
-						<Files
-							data={folder.files}
-							menu={menu}
-							onActiveMenu={onActiveMenu}
-							onActiveModal={onActiveModal}
-						/>
-					)}
-
 					<div className={`upload ${styles.upload}`}>
 						<button
 							type="button"
@@ -65,7 +98,13 @@ export const Folder = () => {
 									<button
 										className={styles['upload-link']}
 										onClick={() =>
-											onActiveModal({ component: <Folder_Create /> })
+											onActiveModal({
+												component: (
+													<Folder_Create
+														onCreateSubfolder={handleCreateSubfolder}
+													/>
+												),
+											})
 										}
 										data-close-menu
 									>
