e02b91b9df78ae15909ca38a77fc3609b7c58870
Weiss Bai <whitesgr03@gmail.com>
2024-10-05 02:52:49 +0800

    feat: if there is no shared file, add conditions for rendering text

--- src/components/pages/Drive/Shared.jsx
+++ src/components/pages/Drive/Shared.jsx
@@ -1,5 +1,5 @@
 // Packages
-import { useOutletContext, Link } from 'react-router-dom';
+import { useOutletContext, Link, useMatch } from 'react-router-dom';
 import { format } from 'date-fns';
 
 // Styles
@@ -11,82 +11,95 @@ import styles from './Shared.module.css';
 import { Shared_Delete } from './Shared_Delete';
 
 export const Shared = () => {
-	const { shared, menu, onActiveMenu, onActiveModal } = useOutletContext();
+	const { shared, menu, onActiveMenu, onActiveModal, onGetSharing } =
+		useOutletContext();
+
+	const matchPath = useMatch('/drive/shared');
 
 	return (
 		<>
-			<h3>Shared</h3>
-			<ul className={driveStyles.list}>
-				{shared.map(item => (
-					<li key={item.file.id} className={driveStyles.item}>
-						<Link
-							to={`/drive/shared/${item.file.id}`}
-							className={driveStyles.container}
-						>
-							<span className={`${icon} ${driveStyles.image}`} />
-							{/* file.type icon */}
-							<div className={driveStyles.content}>
-								<p className={driveStyles.name}>{item.file.name}</p>
-								<div className={driveStyles['info-wrap']}>
-									<div className={driveStyles.info}>
-										<span className={`${icon} ${styles['share-by']}`} />
-										<span className={driveStyles['file-content']}>
-											{item.file.owner.username}
-										</span>
+			{shared.length !== 0 ? (
+				<>
+					<h3>Shared with you</h3>
+					<ul className={driveStyles.list}>
+						{shared.map(item => (
+							<li key={item.file.id} className={driveStyles.item}>
+								<Link
+									to={`/drive/shared/${item.file.id}`}
+									className={driveStyles.container}
+								>
+									<span className={`${icon} ${driveStyles.image}`} />
+									{/* file.type icon */}
+									<div className={driveStyles.content}>
+										<p className={driveStyles.name}>{item.file.name}</p>
+										<div className={driveStyles['info-wrap']}>
+											<div className={driveStyles.info}>
+												<span className={`${icon} ${styles['share-by']}`} />
+												<span className={driveStyles['file-content']}>
+													{item.file.owner.username}
+												</span>
+											</div>
+											<div className={driveStyles.info}>
+												<span className={`${icon} ${driveStyles.calendar}`} />
+												<span>
+													{format(item.members[0].sharedAt, 'MMM d, y')}
+												</span>
+											</div>
+										</div>
 									</div>
-									<div className={driveStyles.info}>
-										<span className={`${icon} ${driveStyles.calendar}`} />
-										<span>{format(item.members[0].sharedAt, 'MMM d, y')}</span>
-									</div>
-								</div>
-							</div>
-						</Link>
-						<div className={driveStyles.options}>
-							<button
-								onClick={() =>
-									onActiveMenu({
-										id: item.id,
-										button: 'option-button',
-										name: 'option-menu',
-									})
-								}
-								data-id={item.id}
-								data-button="option-button"
-							>
-								<span className={`${icon} ${driveStyles.option}`} />
-							</button>
-							{menu.name === 'option-menu' && menu.id === item.id && (
-								<ul className={`option-menu ${driveStyles['option-menu']}`}>
-									{item.file.download_url && (
-										<li>
-											<a
-												className={driveStyles['option-menu-button']}
-												href={item.file.download_url}
-												download={item.file.name}
-											>
-												<span className={`${icon} ${driveStyles.download}`} />
-												Download
-											</a>
-										</li>
+								</Link>
+								<div className={driveStyles.options}>
+									<button
+										onClick={() =>
+											onActiveMenu({
+												id: item.id,
+												button: 'option-button',
+												name: 'option-menu',
+											})
+										}
+										data-id={item.id}
+										data-button="option-button"
+									>
+										<span className={`${icon} ${driveStyles.option}`} />
+									</button>
+									{menu.name === 'option-menu' && menu.id === item.id && (
+										<ul className={`option-menu ${driveStyles['option-menu']}`}>
+											{item.file.download_url && (
+												<li>
+													<a
+														className={driveStyles['option-menu-button']}
+														href={item.file.download_url}
+														download={item.file.name}
+													>
+														<span
+															className={`${icon} ${driveStyles.download}`}
+														/>
+														Download
+													</a>
+												</li>
+											)}
+											<li>
+												<button
+													type="button"
+													className={driveStyles['option-menu-button']}
+													}
+												>
+													<span className={`${icon} ${driveStyles.unshare}`} />
+													Unshare
+												</button>
+											</li>
+										</ul>
 									)}
-									<li>
-										<button
-											type="button"
-											className={driveStyles['option-menu-button']}
-											onClick={() =>
-												onActiveModal(<File_Delete name={item.name} />)
-											}
-										>
-											<span className={`${icon} ${driveStyles.delete}`} />
-											Remove
-										</button>
-									</li>
-								</ul>
-							)}
-						</div>
-					</li>
-				))}
-			</ul>
+								</div>
+							</li>
+						))}
+					</ul>
+				</>
+			) : (
+				matchPath && (
+					<p className={styles.text}>There are not files shared with you</p>
+				)
+			)}
 		</>
 	);
 };
