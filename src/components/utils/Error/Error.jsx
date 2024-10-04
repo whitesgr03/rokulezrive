3e8d9635a6649a5ff646e26c4e1fc518cfe6af39
Weiss Bai <whitesgr03@gmail.com>
2024-10-04 18:27:58 +0800

    feat: add darkThem state and "getColorTheme" function

--- src/components/utils/Error/Error.jsx
+++ src/components/utils/Error/Error.jsx
@@ -1,16 +1,38 @@
 // Packages
 import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 // Styles
 import styles from './Error.module.css';
 import { icon } from '../../../styles/icon.module.css';
 
-export const Error = ({ error }) => {
+	const [darkTheme, setDarkTheme] = useState(false);
 	const { state } = useLocation();
+
 	console.error('Error component:', state ? state.error : error);
 
+	useEffect(() => {
+		const getColorTheme = () => {
+			const darkScheme = localStorage.getItem('darkTheme');
+
+			const browserDarkScheme =
+				window.matchMedia('(prefers-color-scheme: dark)')?.matches ?? false;
+
+			darkScheme === null &&
+				localStorage.setItem('darkTheme', browserDarkScheme);
+
+			console.log(darkScheme);
+
+			setDarkTheme(
+				darkScheme === null ? browserDarkScheme : darkScheme === 'true',
+			);
+		};
+		!children && getColorTheme();
+	}, [children]);
+
 	return (
+		<div className={`${styles.container} ${darkTheme ? 'dark' : ''}`}>
 			<div className={styles.error}>
 				<span className={`${icon} ${styles.alert}`} />
 				<div className={styles.message}>
@@ -18,6 +40,7 @@ export const Error = ({ error }) => {
 						<p>
 							Please come back later, or if you have any questions, contact us.
 						</p>
+				</div>
 				<Link to="/" className={styles.link}>
 					Back to Home Page
 				</Link>
