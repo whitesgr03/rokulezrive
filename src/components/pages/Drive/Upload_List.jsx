80e0cb471212986b42acbb394bd60c1af8fd5cb1
Weiss Bai <whitesgr03@gmail.com>
2024-10-14 23:54:26 +0800

    feat: create upload list component

---
+++ src/components/pages/Drive/Upload_List.jsx
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types';
+
+import { Folder_Create } from './Folder/Subfolder/Folder_Create';
+import { File_Upload } from './Folder/File/File_Upload';
+
+import { icon } from '../../../styles/icon.module.css';
+import styles from './Upload_List.module.css';
+
+export const Upload_List = ({
+	folder,
+	onActiveModal,
+	onAddFolder,
+	onGetFolder,
+}) => {
+	return (
+		<ul className={`upload-menu ${styles['upload-menu']}`}>
+			<li className={styles['upload-item']}>
+				<button
+					className={styles['upload-link']}
+					onClick={() =>
+						onActiveModal({
+							component: (
+								<File_Upload
+									folderId={folder.id}
+									onGetFolder={onGetFolder}
+									onActiveModal={onActiveModal}
+								/>
+							),
+						})
+					}
+					data-close-menu
+				>
+					<span className={`${icon} ${styles['upload-file']}`} />
+					Upload File
+				</button>
+			</li>
+			<li className={styles['upload-item']}>
+				<button
+					className={styles['upload-link']}
+					onClick={() =>
+						onActiveModal({
+							component: (
+								<Folder_Create
+									parentId={folder.id}
+									onAddFolder={onAddFolder}
+									onActiveModal={onActiveModal}
+								/>
+							),
+						})
+					}
+					data-close-menu
+				>
+					<span className={`${icon} ${styles['create-folder']}`} />
+					Create Folder
+				</button>
+			</li>
+		</ul>
+	);
+};
+
+Upload_List.propTypes = {
+	folder: PropTypes.object,
+	onActiveModal: PropTypes.func,
+	onAddFolder: PropTypes.func,
+	onGetFolder: PropTypes.func,
+};
