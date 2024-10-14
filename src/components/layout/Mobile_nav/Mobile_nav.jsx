9b637618dd66331fd491cf725923be793001f651
Weiss Bai <whitesgr03@gmail.com>
2024-10-14 17:17:06 +0800

    feat!: replace Link component with NavLink component

--- src/components/layout/Mobile_nav/Mobile_nav.jsx
+++ src/components/layout/Mobile_nav/Mobile_nav.jsx
@@ -1,34 +1,37 @@
 // Packages
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 // Styles
 import styles from './Mobile_Nav.module.css';
 import { icon } from '../../../styles/icon.module.css';
 
-export const Mobile_Nav = () => (
-	<nav className={styles['mobile-nav']}>
-		<ul className={styles['mobile-nav-list']}>
-			<li className={styles['mobile-nav-item']}>
-				<Link to="/drive" className={styles['mobile-nav-link']}>
-					<span className={`${icon} ${styles.home}`} />
-					Home
-				</Link>
-			</li>
-			<li className={styles['mobile-nav-item']}>
-				<Link to="/drive/shared" className={styles['mobile-nav-link']}>
-					<span className={`${icon} ${styles.shared}`} />
-					Shared
-				</Link>
-			</li>
-			<li className={styles['mobile-nav-item']}>
-				<Link
-					to="/drive/folders/my-drive"
-					className={styles['mobile-nav-link']}
-				>
-					<span className={`${icon} ${styles.drive}`} />
-					Files
-				</Link>
-			</li>
-		</ul>
-	</nav>
-);
+export const Mobile_Nav = () => {
+	const activeLink = ({ isActive }) =>
+		isActive
+			? `${styles['mobile-nav-link']} ${styles.active}`
+			: `${styles['mobile-nav-link']}`;
+	return (
+		<nav className={styles['mobile-nav']}>
+			<ul className={styles['mobile-nav-list']}>
+				<li className={styles['mobile-nav-item']}>
+					<NavLink to="/drive" end className={activeLink}>
+						<span className={`${icon} ${styles.home}`} />
+						Home
+					</NavLink>
+				</li>
+				<li className={styles['mobile-nav-item']}>
+					<NavLink to="/drive/shared" end className={activeLink}>
+						<span className={`${icon} ${styles.shared}`} />
+						Shared
+					</NavLink>
+				</li>
+				<li className={styles['mobile-nav-item']}>
+					<NavLink to="/drive/folders/my-drive" end className={activeLink}>
+						<span className={`${icon} ${styles.drive}`} />
+						Files
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
