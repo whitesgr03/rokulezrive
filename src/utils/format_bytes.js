d92062773b0162eac7922072ee9341f266301635
Weiss Bai <whitesgr03@gmail.com>
2024-10-17 17:10:04 +0800

    feat: add GB and TB sizes

--- src/utils/format_bytes.js
+++ src/utils/format_bytes.js
@@ -1,6 +1,6 @@
 export const formatBytes = bytes => {
 	const k = 1000;
-	const sizes = ['Bytes', 'KB', 'MB'];
+	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
 
 	const i = Math.floor(Math.log(bytes) / Math.log(k));
 
