ad3ab7f4a021a470b7445e0aa078fbf3730b34a8
Weiss Bai <whitesgr03@gmail.com>
2024-10-29 21:09:39 +0800

    feat: when downloading the file, get the download URL instead of creating a copy of the file

--- src/components/pages/Drive/Shared_File.jsx
+++ src/components/pages/Drive/Shared_File.jsx
@@ -2,6 +2,7 @@
 import { useOutletContext, useParams, Navigate } from 'react-router-dom';
 import { format } from 'date-fns';
 import { useState } from 'react';
+import { supabase } from '../../../utils/supabase_client';
 
 // Styles
 import { icon } from '../../../styles/icon.module.css';
@@ -22,26 +23,9 @@ export const Shared_File = () => {
 	const downloading = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3Cg fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'%3E%3Cpath stroke-dasharray='2 4' stroke-dashoffset='6' d='M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9'%3E%3Canimate attributeName='stroke-dashoffset' dur='0.6s' repeatCount='indefinite' values='6;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='32' stroke-dashoffset='32' d='M12 21c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.1s' dur='0.4s' values='32;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='10' stroke-dashoffset='10' d='M12 8v7.5'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.5s' dur='0.2s' values='10;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='6' stroke-dashoffset='6' d='M12 15.5l3.5 -3.5M12 15.5l-3.5 -3.5'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.7s' dur='0.2s' values='6;0'/%3E%3C/path%3E%3C/g%3E%3C/svg%3E#123#${Math.random()}")`;
 
 	const handleGetResourceUrl = async ({ id, name }) => {
-		const deleteResource = async data => {
-			const url = `https://api.cloudinary.com/v1_1/${data.cloud_name}/delete_by_token`;
-
-			const options = {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					token: data.delete_token,
-				}),
-			};
-
-			const { result } = await handleFetch(url, options);
-			result !== 'ok' && setError('Fail to delete resource');
-		};
-
-		const createDownloadUrl = async url => {
+		const download = async url => {
 			const blob = await new Promise(resolve =>
-				fetch(url, { cache: 'no-cache' })
+				fetch(url)
 					.then(res => resolve(res.blob()))
 					.catch(() => resolve(null)),
 			);
@@ -53,30 +37,34 @@ export const Shared_File = () => {
 				a.click();
 			};
 
-			!blob?.size
+			!blob
 				? setError('File resource url cloud not be loaded')
 				: handleDownload(URL.createObjectURL(blob));
 		};
 
 		const createResourceUrl = async () => {
 			setLoading(true);
-			const url = `${import.meta.env.VITE_RESOURCE_URL}/api/files/${id}/copy`;
+
+			const {
+				data: {
+					session: { access_token },
+				},
+			} = await supabase.auth.getSession();
+
+			const url = `${import.meta.env.VITE_RESOURCE_URL}/api/files/${id}/download-url`;
 
 			const options = {
-				method: 'POST',
+				method: 'GET',
 				headers: {
-					'X-Requested-With': 'XmlHttpRequest',
+					Authorization: `Bearer ${access_token}`,
 				},
-				credentials: 'include',
 			};
 
 			const result = await handleFetch(url, options);
 
 			const handleSuccess = async () => {
-				const { secure_url, ...data } = result.data;
-				await createDownloadUrl(secure_url);
+				await download(result.data.url);
 				setLoading(false);
-				await deleteResource(data);
 			};
 
 			result.success ? handleSuccess() : setError(result.message);
