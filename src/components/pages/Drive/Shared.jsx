ffe12841861ac46ce8c86438eb94324b64edf463
Weiss Bai <whitesgr03@gmail.com>
2024-10-31 16:20:55 +0800

    feat: reset SVG animation every time download of file is triggered

--- src/components/pages/Drive/Shared.jsx
+++ src/components/pages/Drive/Shared.jsx
@@ -23,15 +23,21 @@ const RESOURCE_URL =
 		: import.meta.env.VITE_LOCAL_RESOURCE_URL;
 
 export const Shared = () => {
-	const { shared, menu, onActiveMenu, onActiveModal, onDeleteSharedFile } =
-		useOutletContext();
+	const {
+		shared,
+		menu,
+		onActiveMenu,
+		onActiveModal,
+		onDeleteSharedFile,
+		downloading,
+		onResetSVGAnimate,
+	} = useOutletContext();
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
 
 	const isNormalTablet = useMediaQuery({ minWidth: 700 });
 
 	const matchPath = useMatch('/drive/shared');
-	const downloading = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3Cg fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'%3E%3Cpath stroke-dasharray='2 4' stroke-dashoffset='6' d='M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9'%3E%3Canimate attributeName='stroke-dashoffset' dur='0.6s' repeatCount='indefinite' values='6;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='32' stroke-dashoffset='32' d='M12 21c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.1s' dur='0.4s' values='32;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='10' stroke-dashoffset='10' d='M12 8v7.5'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.5s' dur='0.2s' values='10;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='6' stroke-dashoffset='6' d='M12 15.5l3.5 -3.5M12 15.5l-3.5 -3.5'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.7s' dur='0.2s' values='6;0'/%3E%3C/path%3E%3C/g%3E%3C/svg%3E#123#${Math.random()}")`;
 
 	const handleGetResourceUrl = async ({ id, name }) => {
 		const download = async url => {
@@ -77,6 +83,7 @@ export const Shared = () => {
 				await download(result.data.url);
 				onActiveMenu();
 				setLoading(false);
+				onResetSVGAnimate();
 			};
 
 			result.success ? handleSuccess() : setError(result.message);
