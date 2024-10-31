7c25e7f438c40de3a29bbb8969ead13af7e6a9d9
Weiss Bai <whitesgr03@gmail.com>
2024-10-31 16:17:53 +0800

    feat: create handleResetSVGAnimate

--- src/components/pages/Drive/Drive.jsx
+++ src/components/pages/Drive/Drive.jsx
@@ -25,6 +25,7 @@ import { Footer } from '../../layout/Footer/Footer';
 import { handleFetch } from '../../../utils/handle_fetch';
 
 // Variables
+const downloadingIcon = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3Cg fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'%3E%3Cpath stroke-dasharray='2 4' stroke-dashoffset='6' d='M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9'%3E%3Canimate attributeName='stroke-dashoffset' dur='0.6s' repeatCount='indefinite' values='6;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='32' stroke-dashoffset='32' d='M12 21c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.1s' dur='0.4s' values='32;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='10' stroke-dashoffset='10' d='M12 8v7.5'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.5s' dur='0.2s' values='10;0'/%3E%3C/path%3E%3Cpath stroke-dasharray='6' stroke-dashoffset='6' d='M12 15.5l3.5 -3.5M12 15.5l-3.5 -3.5'%3E%3Canimate fill='freeze' attributeName='stroke-dashoffset' begin='0.7s' dur='0.2s' values='6;0'/%3E%3C/path%3E%3C/g%3E%3C/svg%3E#123`;
 const RESOURCE_URL =
 	import.meta.env.MODE === 'production'
 		? import.meta.env.VITE_RESOURCE_URL
@@ -43,10 +44,15 @@ export const Drive = () => {
 
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
+	const [downloading, setDownloading] = useState(`url("${downloadingIcon}")`);
 
 	const folder = folderId
 		? folders.find(folder => folder.id === folderId)
 		: folders[0];
+
+	const handleResetSVGAnimate = () => {
+		setDownloading(`url("${downloadingIcon}#${Math.random()}")`);
+	};
 
 	const handleUpdateFolder = ({ parentFolder, currentFolder, newFolder }) => {
 		const newFolders = folders.map(
@@ -218,6 +224,8 @@ export const Drive = () => {
 												onUpdateFolder: handleUpdateFolder,
 												onDeleteSharedFile: handleDeleteSharedFile,
 												menu,
+												downloading: downloading,
+												onResetSVG: handleResetSVGAnimate,
 											}}
 										/>
 									</div>
