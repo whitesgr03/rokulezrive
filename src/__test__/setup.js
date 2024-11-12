12662edb15de37e17a0abbe4263ab0a4d02ba14d
Weiss Bai <whitesgr03@gmail.com>
2024-11-13 00:51:32 +0800

    feat: add setup file

---
+++ src/__test__/setup.js
@@ -0,0 +1,10 @@
+import { vi, expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from '@testing-library/jest-dom/matchers';
+
+expect.extend(matchers);
+
+afterEach(() => {
+	vi.clearAllMocks();
+	cleanup();
+});
