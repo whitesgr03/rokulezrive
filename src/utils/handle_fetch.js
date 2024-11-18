6682b355b01a8296142f6c846a212a2d087fee84
Weiss Bai <whitesgr03@gmail.com>
2024-11-18 13:57:15 +0800

    feat: create handleFetchBlob to get resource blob

--- src/utils/handle_fetch.js
+++ src/utils/handle_fetch.js
@@ -12,3 +12,17 @@ export const handleFetch = async (url, option) => {
 	}
 	return result;
 };
+
+export const handleFetchBlob = async url => {
+	let result = null;
+	try {
+		const response = await fetch(url);
+		result = { success: true, blob: await response.blob() };
+	} catch (err) {
+		result = {
+			success: false,
+			message: err,
+		};
+	}
+	return result;
+};
